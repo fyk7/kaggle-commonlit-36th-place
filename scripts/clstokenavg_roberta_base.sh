3f1f72411d6c42d6efbda6f2f5786d04c419e66f
fyk7 <tfscofield115@gmail.com>
2022-01-14 18:37:57 +0900

    add run scripts

--- scripts/clstokenavg_roberta_base.sh
+++ scripts/clstokenavg_roberta_base.sh
@@ -1,5 +1,5 @@
 python3 src/main.py \
-model_class="LitModel" \
+model_class="ClsTokenAvgModel" \
 model_path="roberta-base" \
 tokenizer_path="roberta-base" \
 batch_size=16 \
---
+++ scripts/roberta_large.sh
@@ -0,0 +1,7 @@
+python3 src/main.py \
+optimizer=large \
+model_class="LitModel" \
+model_path="roberta-large" \
+tokenizer_path="roberta-large" \
+batch_size=8 \
+seed=1000
